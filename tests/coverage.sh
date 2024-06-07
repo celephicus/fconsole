b0b0e4d94d18bae9340bb05c91818cb5e2ad3b7f
tomh@dedalus <celephicus@gmail.com>
2024-06-07 17:27:20 +1000

    Added tests for hex string. Redid test harness preparatory to putting it in a header.

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,3 +1,2 @@
-make clean && \
-make GCOV_CFLAGS="--coverage -O0" GCOV_LDFLAGS="-lgcov --coverage" && \
-gcovr -f ../src/console.c -f main.c --html --html-details -o coverage/coverage.html
+gcovr -f ../src/console.c -f main.c --html --html-details -o coverage/coverage.html 
+open coverage/coverage.html 
